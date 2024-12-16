fcf7c392fcc9ee7d391aa83beab4e1217188ceb1
Hyegyeong <gju06061@naver.com>
2024-12-16 19:01:13 +0900

    feat: 피드백 전송 성공 시 자동 홈화면 이동 구현

--- app/src/main/java/people/droid/untitled/ui/FeedbackScreen.kt
+++ app/src/main/java/people/droid/untitled/ui/FeedbackScreen.kt
@@ -2,14 +2,12 @@ package people.droid.untitled.ui
 
 import android.content.Context
 import android.widget.Toast
-import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,8 +17,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -35,10 +37,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.delay
 import people.droid.untitled.R
 import people.droid.untitled.ui.theme.StoryMilkyFontFamilyBold
 import people.droid.untitled.ui.theme.UntitledTheme
@@ -47,19 +51,24 @@ import people.droid.untitled.ui.theme.YellowBackground
 const val FEEDBACK_ROUTE = "feedback"
 
 @Composable
-fun FeedbackScreen(viewModel: FeedbackViewModel, popBackStack:()->Unit) {
+fun FeedbackScreen(viewModel: FeedbackViewModel, goToHome: () -> Unit) {
     var text by remember { mutableStateOf("") }
+    var isSendMessageScreenShown by remember { mutableStateOf(true) }
     val uiState = viewModel.uiState.collectAsStateWithLifecycle().value
     val context = LocalContext.current
-    BackHandler {
-        viewModel.resetUiState()
-        popBackStack()
+    LaunchedEffect(isSendMessageScreenShown) {
+        if (!isSendMessageScreenShown) {
+            delay(1500)
+            goToHome()
+        }
     }
     FeedbackScreenUI(
         context,
+        isSendMessageScreenShown,
+        { isSendMessageScreenShown = it },
         uiState,
-        viewModel::resetUiState,
         text,
+        goToHome,
         { text = it },
     ) {
         viewModel.postFeedback(text)
@@ -70,22 +79,27 @@ fun FeedbackScreen(viewModel: FeedbackViewModel, popBackStack:()->Unit) {
 @Composable
 fun FeedbackScreenUI(
     context: Context,
+    isSendMessageScreenShown: Boolean,
+    onSendMessageScreenShownChanged: (Boolean) -> Unit,
     uiState: FeedbackUiState = FeedbackUiState(),
-    resetUiState: () -> Unit = {},
     text: String = "",
+    goToHome: () -> Unit = {},
     onTextChange: (String) -> Unit = {},
-    onSendButtonClicked: () -> Unit = {}
+    onSendButtonClicked: () -> Unit = {},
 ) {
-    var showSendMessageScreen by remember { mutableStateOf(false) }
     LaunchedEffect(uiState.isSuccess) {
-        showSendMessageScreen = when (uiState.isSuccess) {
-            true -> false
+        when (uiState.isSuccess) {
+            true -> onSendMessageScreenShownChanged(false)
             false -> {
-                Toast.makeText(context, "Submission failed... Please try again.", Toast.LENGTH_SHORT).show()
-                true
+                Toast.makeText(
+                    context,
+                    context.getString(R.string.fail_message_feedback_submission),
+                    Toast.LENGTH_SHORT
+                ).show()
+                onSendMessageScreenShownChanged(true)
             }
 
-            else -> true
+            else -> onSendMessageScreenShownChanged(true)
         }
     }
     UntitledTheme {
@@ -96,14 +110,10 @@ fun FeedbackScreenUI(
                     .fillMaxSize()
                     .padding(innerPadding)
             ) {
-                if (showSendMessageScreen) {
-                    SendMessageScreen(text, onTextChange, onSendButtonClicked)
+                if (isSendMessageScreenShown) {
+                    SendMessageScreen(text, onTextChange, onSendButtonClicked, goToHome)
                 } else {
-                    CompleteSendMessageScreen {
-                        showSendMessageScreen = true
-                        resetUiState()
-                        onTextChange("")
-                    }
+                    CompleteSendMessageScreen()
                 }
 
             }
@@ -113,12 +123,26 @@ fun FeedbackScreenUI(
 }
 
 @Composable
-fun SendMessageScreen(message: String = "", onTextChange: (String) -> Unit = {}, onSendButtonClicked: () -> Unit = {}) {
+fun SendMessageScreen(
+    message: String = "",
+    onTextChange: (String) -> Unit = {},
+    onSendButtonClicked: () -> Unit = {},
+    goToHome: () -> Unit = {}
+) {
     Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        IconButton(
+            modifier = Modifier.align(Alignment.Start),
+            onClick = goToHome,
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = "Back"
+            )
+        }
         Spacer(Modifier.height(50.dp))
-        Text("Your Ideas", style = MaterialTheme.typography.titleLarge)
+        Text(stringResource(R.string.label_feedback), style = MaterialTheme.typography.titleLarge)
         Text(
-            "Let us know what features you'd like to see!\nWe'll do our best to add them!",
+            stringResource(R.string.description_feedback),
             textAlign = TextAlign.Center
         )
         BasicTextField(
@@ -141,20 +165,23 @@ fun SendMessageScreen(message: String = "", onTextChange: (String) -> Unit = {},
             onClick = onSendButtonClicked,
             colors = ButtonDefaults.buttonColors(contentColor = Color.White, containerColor = Color.Black)
         ) {
-            Text("SEND", fontFamily = StoryMilkyFontFamilyBold)
+            Text(stringResource(R.string.label_submit_feedback_button), fontFamily = StoryMilkyFontFamilyBold)
         }
     }
 }
 
 @Composable
-fun CompleteSendMessageScreen(onSendMoreButtonClicked: () -> Unit = {}) {
+fun CompleteSendMessageScreen() {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Spacer(Modifier.height(50.dp))
-        Text("Successfully sent!", style = MaterialTheme.typography.headlineLarge)
+        Text(
+            stringResource(R.string.success_message_feedback_submission),
+            style = MaterialTheme.typography.headlineLarge
+        )
         Image(
             modifier = Modifier
                 .padding(20.dp)
@@ -162,14 +189,6 @@ fun CompleteSendMessageScreen(onSendMoreButtonClicked: () -> Unit = {}) {
             painter = painterResource(R.drawable.checked),
             contentDescription = null
         )
-        Row {
-            Button(
-                onClick = onSendMoreButtonClicked,
-                colors = ButtonDefaults.buttonColors(contentColor = Color.White, containerColor = Color.Black)
-            ) {
-                Text("Send more", fontFamily = StoryMilkyFontFamilyBold)
-            }
-        }
     }
 }
 
@@ -177,7 +196,9 @@ fun CompleteSendMessageScreen(onSendMoreButtonClicked: () -> Unit = {}) {
 @Preview(showBackground = true)
 fun SendMessageScreenPreview() {
     UntitledTheme {
-        SendMessageScreen()
+        Box(modifier = Modifier.fillMaxSize()) {
+            SendMessageScreen()
+        }
     }
 }
 
