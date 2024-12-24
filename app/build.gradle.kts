46a9c478ada305ba96c4150c37adbd15c891b843
yewon-yw <kimyewon0401@gmail.com>
2024-12-24 22:31:09 +0900

    update: 1.0.3
    - 광고가 보이지 않는 문제 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "people.droid.untitled"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
